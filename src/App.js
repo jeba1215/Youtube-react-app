4aca444a4c77bb71244b5ff6d6051fda7896caf6
Jesper <jesper.back.94@gmail.com>
2018-08-02 19:57:56 +0200

    grid of videos is now created properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import YoutubeCardGrid from './YoutubeCard/YoutubeCardGrid';
 
 class App extends Component {
--- src/YoutubeCard/YoutubeCardGrid.js
+++ src/YoutubeCard/YoutubeCardGrid.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import './YouTubeCard.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class YoutubeCard extends Component {
-	constructor(props){
-		super(props)
-	}
-
-	componentDidUpdate(prevProps){
-	}
 
 	render(){
 		return (
 			<div className="YoutubeCard">
 				<div className="thumbnail-with-text">
-					<img src={this.props.videoItem.thumbnails.high.url}></img>
+					<img src={this.props.videoItem.thumbnails.medium.url}></img>
 					<br/>
-					{this.props.videoItem.title}
+					<span className="videotitle"> {this.props.videoItem.title} </span>
+					<br/> 
+					<span className="videochannel"> {this.props.videoItem.channel} </span>
 					<br/>
-					{this.props.videoItem.channel}
-					<br/>
-					{this.props.videoItem.date}
+					<span className="videodate"> {this.props.videoItem.date} </span>
 					<br/>
 				</div>
 			</div>	
@@ -27,28 +22,47 @@ class YoutubeCard extends Component {
 	}
 }
 
-class YoutubeCardGrid extends Component {
-	constructor(props){
-		super(props)
-	}
 
-	componentDidUpdate(prevProps){
-		console.log("videoItem:", this.props.videoItems[0])
+class YoutubeCardGrid extends Component {
+
+	// TODO the last row of videos aren't shown at the moment, fix this later
+	split_videos_into_rows(videos){
+		let columns = 4
+		let num_rows = Math.floor(videos.length / columns)
+		let rest = videos.length % columns
+		let i_item = 0
+		
+		// Iterate over all of the rows 
+		let result_items = []
+		for(let i=0; i<num_rows; i++){
+			let cells = []
+			for(let j=0; j<columns; j++){
+				let colmd = "col-md-" + ( 12 / columns )
+				cells.push((<div className={colmd}> <YoutubeCard key={"cell"+i_item} videoItem={videos[i_item]}></YoutubeCard> </div>))
+				i_item += 1
+			}
+			
+			let row = (<div key={"row"+i_item} className="row">{cells} </div>)
+			result_items.push(row)
+		}
+
+		return (<div className=""> {result_items} </div>)
 	}
 
 	getListOfVideoItems(){
-		return this.props.videoItems.map((item) => {
-			return <li key={item.id}><YoutubeCard videoItem={item}></YoutubeCard></li>
-		})
+		return this.split_videos_into_rows(this.props.videoItems)
 	}
+
 
 	render() {
 		return (
 			<div className="YoutubeCardGrid">
-			<ul>{this.getListOfVideoItems()}</ul>
+				{this.getListOfVideoItems()}
 			</div>
 			);
 	}
+
 }
 
 export default YoutubeCardGrid;
+
